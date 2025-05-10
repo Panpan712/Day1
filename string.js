b31d2f255aeb8f8020e17c25507c7a3e7468bdc9
Panpan712 <panneieikyaw7890@gmail.com>
2025-05-10 09:32:14 +0630

    add file.txt

---
+++ string.js
@@ -0,0 +1,86 @@
+//length
+
+let text = "Hello World";
+console.log(text.length); 
+
+//toUpperCase
+
+let name = "aung aung";
+console.log(name.toUpperCase()); 
+
+//toLowerCase
+
+let greeting = "HELLO";
+console.log(greeting.toLowerCase()); 
+
+//slice(start, end)
+
+let test = "JavaScript";
+console.log(test.slice(0, 4)); 
+
+// substring(start, end)
+
+let Text = "Developer";
+console.log(Text.substring(0, 5)); 
+
+// substr(start, length)
+
+let Test = "Programming";
+console.log(Test.substr(3, 4)); 
+
+// replace(search, replacement)
+
+let str = "I love cats.";
+console.log(str.replace("cats", "dogs")); 
+
+//includes(substring)
+
+let string = "JavaScript is fun";
+console.log(string.includes("fun")); 
+
+//indexOf(substring)
+
+let TEXT = "banana";
+console.log(TEXT.indexOf("a")); 
+
+// lastIndexOf(substring)
+
+let TEST = "banana";
+console.log(TEST.lastIndexOf("a")); 
+
+// trim()
+
+let strings = "   hello   ";
+console.log(strings.trim()); 
+
+// split(separator)
+
+let fruits = "apple,banana,mango";
+console.log(fruits.split(","));
+
+//concat()
+
+let a = "Hello";
+let b = "World";
+console.log(a.concat(" ", b)); 
+
+// startsWith(substring)
+
+let greet = "JavaScript";
+console.log(greet.startsWith("Java")); 
+
+// endsWith(substring)
+
+let word = "backend.js";
+console.log(word.endsWith(".js")); 
+
+//charAt(index)
+
+let Greet = "Myanmar";
+console.log(Greet.charAt(2));
+
+//repeat(count)
+
+let Word = "ha";
+console.log(Word.repeat(3));
+
